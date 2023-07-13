c8759fd7d566d949a2966ffd88286cb63d7ab9d1
yudixie <yudixie235@gmail.com>
2023-07-13 14:52:57 -0400

    update test

--- controllers/test.py
+++ controllers/test.py
@@ -8,6 +8,7 @@ Usage: `python3 test.py`
 """
 
 if __name__ == "__main__":
-    c = ImageDataset(train=20000, val=50, library="models_full.json", materials=True, hdri=False, overwrite=True,
-                     do_zip=True, random_seed=0, output_directory=Path.home().joinpath("tdw_image_dataset_test"))
+    c = ImageDataset(train=20000, val=50, library="models_core.json", materials=True, hdri=False, overwrite=True,
+                     do_zip=True, random_seed=0, output_directory=Path.home().joinpath("tdw_image_dataset_test"),
+                     launch_build=True)
     c.run(scene_name="tdw_room")
