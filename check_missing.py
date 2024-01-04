4043753980678b2a32b4d247972b0df04ca3e784
YudiXie <yudixie235@gmail.com>
2024-01-03 19:31:00 -0500

    update use pathlib

--- check_missing.py
+++ check_missing.py
@@ -1,4 +1,3 @@
-import os
 import argparse
 import sys
 from pathlib import Path
@@ -8,26 +7,25 @@ import pandas as pd
 
 
 if __name__ == '__main__':
-    # parser = argparse.ArgumentParser()
-    # parser.add_argument('-d', '--directory', default='', help='the directory to saved dataset')
-    # # eg. '/om2/user/yu_xie/data/tdw_images/tdw_image_dataset_small_multi_env_hdri'
-    # args = parser.parse_args()
-    # dataset_folder = args.directory
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--index', default='', help='the path to saved index')
+    # eg. '/om2/user/yu_xie/data/tdw_images/tdw_image_dataset_small_multi_env_hdri/index_img_5898.csv'
+    args = parser.parse_args()
 
-    dataset_folder = '/om2/user/yu_xie/data/tdw_images/tdw_image_dataset_small_multi_env_hdri'
+    index_path = Path(args.index)
+    dataset_path = index_path.parent
 
-    index_file = os.path.join(dataset_folder, 'index_img_5898.csv')
-    full_df = pd.read_csv(index_file, index_col=0)
+    full_df = pd.read_csv(index_path, index_col=0)
 
     missing_idx = []
     for idx in trange(len(full_df)):
         row = full_df.iloc[idx]
-        image_folder = os.path.join(dataset_folder, 'images', row['scene'], row['wnid'], row['model'])
-        image_path = os.path.join(image_folder, f'img_img_{idx:010d}.jpg')
-        image_meta_path = os.path.join(image_folder, f'img_{idx:010d}_info.csv')
+        image_folder = dataset_path.joinpath('images', row['scene'], row['wnid'], row['model'])
+        image_path = image_folder.joinpath(f'img_img_{idx:010d}.jpg')
+        image_meta_path = image_folder.joinpath(f'img_{idx:010d}_info.csv')
 
         # Check if the image file and the image meta file exists
-        if not os.path.exists(image_path) or not os.path.exists(image_meta_path):
+        if not image_path.is_file() or not image_meta_path.is_file():
             missing_idx.append(idx)
 
     # Convert the list of missing images to a DataFrame
@@ -38,7 +36,7 @@ if __name__ == '__main__':
         sys.exit("exit program.")
     
     print(f"Missing {len(missing_df)} images.")
-    missing_df.to_csv(os.path.join(dataset_folder, 'missing.csv'))
+    missing_df.to_csv(dataset_path.joinpath('missing.csv'))
 
     if input('Continue to modify processed_records? (yes/no): ') != 'yes':
         sys.exit("exit program.")
@@ -47,7 +45,7 @@ if __name__ == '__main__':
         scene_df = missing_df[missing_df['scene'] == scene_name]
         undo_models = scene_df['model'].unique()
 
-        done_models_path: Path = Path(dataset_folder).joinpath(f"{scene_name}_processed_records.txt")
+        done_models_path = dataset_path.joinpath(f"{scene_name}_processed_records.txt")
         processed_model_names = done_models_path.read_text(encoding="utf-8").split("\n")
 
         # remove undo_models from processed_model_names
