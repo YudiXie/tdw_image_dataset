fdce2c2ade859278bd7c8057aafb1aac5221afe0
YudiXie <yudixie235@gmail.com>
2024-06-15 20:58:21 -0400

    bug fix

--- 0616_check_tdw100m_missing_submit.sh
+++ 0616_check_tdw100m_missing_submit.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 for i in /om/user/yu_xie/data/tdw_images/tdw_image_dataset_100m_20240222/images/*.zip
 do
-  sbatch 0616_check_tdw100m_missing.sh "`basename $1i .zip`"
+  sbatch 0616_check_tdw100m_missing.sh "`basename $i .zip`"
 done
